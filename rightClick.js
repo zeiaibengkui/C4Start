11e4aae619d48a0670fc9b68b0a859e01a039b83
zeiaibengkui <2243925461@qqcom>
2024-04-03 23:03:10 +0800

    modified:   rightClick.js

--- rightClick.js
+++ rightClick.js
@@ -5,21 +5,27 @@
 if (!$('#drag')[0].style) $('#drag')[0].setAttritube("style", "");
 
 document.addEventListener('contextmenu', (event) => {
-    event.preventDefault();
-    event.target.parentNode.appendChild($('#rcMenu')[0]);
     /* if (event.target.parentNode.id) {
         console.log($("#" + event.target.parentNode.id));
         $("#" + event.target.parentNode.id).draggable({ handle: "#drag" });
     } */
-    if ($(event.target).hasClass('ui-draggable') || $(event.target.parentNode).hasClass('ui-draggable')) {
-        $("#drag")[0].style.display = "";
+    if (!(event.target.id === "rcMenu" || event.target.parentNode.id === "rcMenu")) {
+        event.preventDefault();
+        event.target.parentNode.appendChild($('#rcMenu')[0]);
+        $('#rcMenu')[0].style.top = event.pageY - event.target.parentNode.offsetTop + 'px';
+        $('#rcMenu')[0].style.left = event.pageX - event.target.parentNode.offsetLeft + 'px';
+        if ($(event.target).hasClass('ui-draggable') || $(event.target.parentNode).hasClass('ui-draggable')) {
+            $("#drag")[0].style.display = "";
+        } else {
+            $("#drag")[0].style.display = "none";
+        }
+        $('#rcMenu').show('quick');
     } else {
-        $("#drag")[0].style.display = "none";
+        //$('#rcMenu')[0].style.display = 'none';
+        //$('#rcMenu').hide('quick');
     }
-    $('#rcMenu')[0].style.top = event.pageY - event.target.parentNode.offsetTop + 'px';
-    $('#rcMenu')[0].style.left = event.pageX - event.target.parentNode.offsetLeft + 'px';
-    console.log(event.target.parentNode.pageY);
-    $('#rcMenu').show('quick');
+
+    //console.log(event.target.parentNode.pageY);
     // console.log('aaa');
 })
 document.addEventListener('click', () => {
@@ -32,6 +38,21 @@ document.addEventListener('click', () => {
 
 $(".drag").draggable({ handle: "#drag" });
 
+//auto save position
+/* document.addEventListener("DOMContentLoaded", (event) => {
+    localforage.getItem(key).then((value) => {
+        if (value) { vl = value }
+        else { localforage.setItem(key, vl); };
+    })
+});
+
+window.addEventListener("beforeunload", (event) => {
+    // Cancel the event as stated by the standard.
+    //event.preventDefault();
+    // Chrome requires returnValue to be set.
+    //event.returnValue = "";
+    localforage.setItem(key, vl);
+}); */
 
 
 logTime();
